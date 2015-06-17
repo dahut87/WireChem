89523367b9430341af487cb3938354cdaac3c410
Nicolas Hordé <nicolas@palon.fr>
2015-06-18 00:28:35 +0200

    feat: ajout de la gestion des préférences gestion de la journalisation

--- desktop/src/fr/evolving/game/desktop/DesktopLauncher.java
+++ desktop/src/fr/evolving/game/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package fr.evolving.game.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
@@ -9,13 +10,10 @@ import fr.evolving.game.main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		//config.fullscreen = true;
-		//config.width = 1920;
-		//config.height = 1080;
-		
 		config.fullscreen = false;
-		config.width = AssetLoader.width;
-		config.height = AssetLoader.height;
+		config.width = 1024;
+		config.height = 768;
 		new LwjglApplication(new main(), config);
+		Gdx.app.log("****", "Lancement de l'application \'Desktop\' WireChem...");
 	}
 }
