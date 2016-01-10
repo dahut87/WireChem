d85c5316bab7bbb11b8eb9ca4d67a56b6c16a885
Nicolas Hordé <nicolas@palon.fr>
2016-01-10 23:41:30 +0100

    feat: modification de l'affichage vers le plein ecran ajout de permission pour vibration, internet ...

--- android/src/fr/evolving/game/android/AndroidLauncher.java
+++ android/src/fr/evolving/game/android/AndroidLauncher.java
@@ -1,6 +1,10 @@
 package fr.evolving.game.android;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -10,7 +14,16 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useImmersiveMode = true;
+		//cfg.hideStatusBar = true; //set to true by default
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+			getWindow().getDecorView().setSystemUiVisibility(View.STATUS_BAR_VISIBLE);
+			getWindow().getDecorView().setSystemUiVisibility(View.STATUS_BAR_HIDDEN);
+		}
 		initialize(new main(), config);
 	}
 }
